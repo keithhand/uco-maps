96f8e0b95a7994447bf653975928b3efe7d97b11
keith.hand <rkeithhand@gmail.com>
2019-04-24 20:57:11 -0500

    create maps controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'map/index'
   resources :buildings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'buildings#index'
+  root 'map#index'
 end
