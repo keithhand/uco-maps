2b31922917a86555a0bd6c9a281f217f2db9bad6
keith.hand <rkeithhand@gmail.com>
2019-04-25 00:38:50 -0500

    maps is updated with buildings from db

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,9 @@
 class MapController < ApplicationController
   def index
+    @buildings = Building.all
+    @hash = Gmaps4rails.build_markers(@buildings) do |building, marker|
+      marker.lat building.latitude
+      marker.lng building.longitude
+    end
   end
 end
