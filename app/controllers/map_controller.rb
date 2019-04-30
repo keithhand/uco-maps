e7ee7ea6c24d219dd6d8eddba48c9d2129da40a5
keith.hand <rkeithhand@gmail.com>
2019-04-30 00:37:46 -0500

    modify map to look cleaner

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,6 +2,7 @@ class MapController < ApplicationController
   def index
     @buildings = Building.all
     @hash = Gmaps4rails.build_markers(@buildings) do |building, marker|
+      marker.infowindow building.title
       marker.lat building.latitude
       marker.lng building.longitude
     end
