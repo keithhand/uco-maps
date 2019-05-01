57c69e350b1ec322b9c4534c678fd7fcbaabaa92
keith.hand <rkeithhand@gmail.com>
2019-05-01 15:59:28 -0500

    mod doc ready function to turbolinks function

--- app/assets/javascripts/selection-boxes.js
+++ app/assets/javascripts/selection-boxes.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   var selectedStartBuilding;
   $("select#startPos").change(function() {
-    if (selectedStartBuilding)
-      $("select#endPos").children("option:eq(" + selectedStartBuilding + ")").show();
+    $("select#endPos").show();
+    $("select#endPos").children("option:eq(" + selectedStartBuilding + ")").show();
     selectedStartBuilding = $(this).children("option:selected").index();
     $("select#endPos").children("option:eq(" + selectedStartBuilding + ")").hide();
     $("select#endPos").val(0);
   })
 
-  $("#directions").find("div")[0].remove();
+  if ($("#directions").find("div")[0])
+    $("#directions").find("div")[0].remove();
 });
