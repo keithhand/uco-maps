6ac0df0598f87d8f861800cd643756fa4e1a9ca4
keith.hand <rkeithhand@gmail.com>
2019-05-01 18:33:50 -0500

    made search hidden unless both fields are selected

--- app/assets/javascripts/selection-boxes.js
+++ app/assets/javascripts/selection-boxes.js
@@ -2,10 +2,15 @@ $(document).on('turbolinks:load', function() {
   var selectedStartBuilding;
   $("select#startPos").change(function() {
     $("select#endPos").show();
+    $("input").hide();
     $("select#endPos").children("option:eq(" + selectedStartBuilding + ")").show();
     selectedStartBuilding = $(this).children("option:selected").index();
     $("select#endPos").children("option:eq(" + selectedStartBuilding + ")").hide();
     $("select#endPos").val(0);
+  })
+
+  $("select#endPos").change(function() {
+    $("input").show();
   })
 
   if ($("#directions").find("div")[0])
