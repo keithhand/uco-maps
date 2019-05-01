c289b758726e4aab6fac6380b602473dc8c71e2b
keith.hand <rkeithhand@gmail.com>
2019-05-01 13:49:52 -0500

    Calculate shortest distance between all entrances

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,41 +4,66 @@ class MapController < ApplicationController
     @directions = params[:directions]
     @error = params[:error_code]
     @hash = params[:markers]
+    @debugvar = params[:debugvar]
   end
 
   # GET /map
   def search
-    require 'google_maps_service'
-
-    if (!params[:startPos] || !params[:endPos])
+    # Error check to make sure both fields were entered in
+    if (!params[:startBuildingID] || !params[:endBuildingID])
       return redirect_to root_path(error_code: "One or more fields was not selected. Please try again")
     end
 
-    @startBuilding = Building.find(params[:startPos])
-    @endBuilding = Building.find(params[:endPos])
-    
+    # Initialize variables and GMaps
+    require 'google_maps_service'
     gmaps = GoogleMapsService::Client.new(key: ENV['API_KEY'])
+    @startEntrances = Entrance.where(building_id: params[:startBuildingID])
+    @endEntrances = Entrance.where(building_id: params[:endBuildingID])
+    @shortestDistance = -1
+    @shortestStartIndex = 0
+    @shortestEndIndex = 0
+
+    # Find shortest distance between all entrances
+    @startEntrances.each_with_index do |startPoint, startIndex|
+      @endEntrances.each_with_index do |endPoint, endIndex|
+        matrix = gmaps.distance_matrix({lat: startPoint.latitude, lng: startPoint.longitude}, {lat: endPoint.latitude, lng: endPoint.longitude}, mode: 'walking')
+        @distance = matrix[:rows][0][:elements][0][:distance][:value]
+        if (@distance < @shortestDistance || @shortestDistance == -1)
+          @shortestDistance = @distance
+          @shortestStartIndex = startIndex
+          @shortestEndIndex = endIndex
+        end
+      end
+    end
+
+    # Set locations based on values found while cross checking all entrances
+    @startLocation = @startEntrances[@shortestStartIndex]
+    @endLocation = @endEntrances[@shortestEndIndex]
+    
+    # Calculate directions
     @routes = gmaps.directions(
-      "#{@startBuilding.latitude}, #{@startBuilding.longitude}",
-      "#{@endBuilding.latitude}, #{@endBuilding.longitude}",
+      "#{@startLocation.latitude}, #{@startLocation.longitude}",
+      "#{@endLocation.latitude}, #{@endLocation.longitude}",
       mode: 'walking',
       alternatives: false)
 
-    @buildings = [@startBuilding, @endBuilding]
+    # Build the marks for the map based on buildings
+    @buildings = [Building.find(params[:startBuildingID]), Building.find(params[:endBuildingID])]
     @hash = Gmaps4rails.build_markers(@buildings) do |building, marker|
       marker.infowindow building.title
       marker.lat building.latitude
       marker.lng building.longitude
     end
 
+    # Build directions array to pass back to index
     @directions = Array.new(@routes[0][:legs][0][:steps].count) { Array.new(2) }
-
     @routes[0][:legs][0][:steps].each_with_index do |routes, index|
         @directions[index][0] = routes[:html_instructions]
         @directions[index][1] = routes[:distance][:text]
     end
 
-    redirect_to root_path(directions: @directions, markers: @hash)
+    # Redirect back to homepage with information gathered
+    redirect_to root_path(directions: @directions, markers: @hash, debugvar: @shortestStartIndex)
   end
 
 end
